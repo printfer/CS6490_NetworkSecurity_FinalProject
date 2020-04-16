1f3256039610ed3ec7d23ba08c3dc1a9b09179bc
studydude <16911871+printfer@users.noreply.github.com>
2020-04-16 15:47:17 -0600

    update setup and .gitignore

--- iot32/setup.sh
+++ iot32/setup.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-wget --no-check-certificate https://mcfp.felk.cvut.cz/publicDatasets/IoT-23-Dataset/iot_23_datasets_small.tar.gz
+if [[ $1 == "small" ]]; then
+    # download iot_23_datasets_small.tar.gz
+    [[ ! -f iot_23_datasets_small.tar.gz ]] && \
+    echo "[+] start download iot_23_datasets_small.tar.gz" && \
+    wget --no-check-certificate https://mcfp.felk.cvut.cz/publicDatasets/IoT-23-Dataset/iot_23_datasets_small.tar.gz
+    tar -xvzf iot_23_datasets_small.tar.gz
+elif [[ $1 == "full" ]]; then
+    # download iot_23_datasets_small.tar.gz
+    [[ ! -f https://mcfp.felk.cvut.cz/publicDatasets/IoT-23-Dataset/iot_23_datasets_full.tar.gz ]] && \
+        echo "[+] start download iot_23_datasets_full.tar.gz" && \
+    wget --no-check-certificate https://mcfp.felk.cvut.cz/publicDatasets/IoT-23-Dataset/iot_23_datasets_full.tar.gz
+    tar -xvzf iot_23_datasets_full.tar.gz
+else
+    echo "[*] Please select (full/small) dataset"
+fi
